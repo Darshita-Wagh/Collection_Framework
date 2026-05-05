1feeccc009f8c5f467533d401ba29c4b4a49c5c9
Darshita-Wagh <waghdarshita57@gmail.com>
2026-05-05 21:38:28 +0530

    Add files via upload

---
+++ Set2.java
@@ -0,0 +1,16 @@
+package collection_framework;
+import java.io.*;   
+import java.util.*;   
+public class Set2 {   
+    public static void main(String args[])   
+    {   
+        Set<Integer> data = new TreeSet<Integer>();   
+        data.add(31);   
+        data.add(21);   
+        data.add(41);   
+        data.add(11);   
+        data.add(61);   
+        data.add(51);   
+        System.out.println("data: " + data);   
+    }   
+}  
